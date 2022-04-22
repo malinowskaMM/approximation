e7077a8198df453eb6af0c9ae3cb92cb3cff0c35
Magdalena Malinowska <236596@edu.p.lodz.pl>
2022-04-22 20:31:49 +0200

    add exmaples of functions

--- View/src/main/java/com/example/view/HelloController.java
+++ View/src/main/java/com/example/view/HelloController.java
@@ -2,20 +2,19 @@ package com.example.view;
 
 import javafx.fxml.FXML;
 import javafx.scene.web.WebView;
-import javafx.scene.control.Label;
 
 public class HelloController {
 
     @FXML private WebView functionI;
     @FXML private WebView functionII;
     @FXML private WebView functionIII;
-    @FXML private WebView functionVI;
+
 
     @FXML
     private void initialize() {
         functionI.getEngine().loadContent("<math display=\\\"block\\\"><mi>y</mi><mo>=</mo><msup><mi>x</mi><mn>5</mn></msup><msup><mi>+x</mi><mn>4</mn></msup><msup><mi>-2x</mi><mn>3</mn></msup><mi>-3</mi></math>");
-        //trigonometricView.getEngine().loadContent("<math display=\\\"block\\\"><mi>y</mi><mo>=</mo><mi>3sin(x)+cos(2x+1)</mi></math>");
-        //mixedView.getEngine().loadContent("<math display=\\\"block\\\"><mi>y</mi><mo>=</mo><mi>8|sin(x)|</mi><mi>-2</mi><msup><mi>x</mi><mn>2</mn></msup><mi>-3x</mi></math>");
+        functionII.getEngine().loadContent("<math display=\\\"block\\\"><mi>y</mi><mo>=</mo><mi>cos(x)</mi><mi>+</mi><mi>(x+1)sin(x)</mi></math>");
+        functionIII.getEngine().loadContent("<math display=\\\"block\\\"><mi>y</mi><mo>=</mo><mi>|</mi><msup><mi>cos(x)</mi><mn>2</mn></msup><mi>+1</mi><mi>|</mi></math>");
     }
 
 }
